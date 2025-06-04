22364824347eff4acbac87172d76957d7662724f
Mark Holmes <contact@markholm.es>
2025-06-03 19:32:00 -0500

    Add ESLint configuration and CI

--- src/background.js
+++ src/background.js
@@ -1,20 +1,20 @@
-const onlyVideoLog = s => console.log(`[only video] ${s}`)
+const onlyVideoLog = s => console.log(`[only video] ${s}`);
 
 chrome.action.onClicked.addListener(async tab => {
-    onlyVideoLog('clicked a tab')
-    const currentTabId = tab.id
+    onlyVideoLog('clicked a tab');
+    const currentTabId = tab.id;
     try {
         await chrome.scripting.executeScript({
             target: { tabId: currentTabId },
             files: ['foreground.js'],
-        })
-        onlyVideoLog('inserted js')
+        });
+        onlyVideoLog('inserted js');
         await chrome.scripting.insertCSS({
             target: { tabId: currentTabId },
             files: ['only-video.css'],
-        })
-        onlyVideoLog('inserted css')
+        });
+        onlyVideoLog('inserted css');
     } catch (e) {
-        onlyVideoLog(`error: ${e}`)
+        onlyVideoLog(`error: ${e}`);
     }
-})
+});
